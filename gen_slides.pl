8236004c8da22b8821f9ed47c3bd452f7aa1dd5e
Victoria Lacroix <victoria.a.lacroix@gmail.com>
2015-06-19 15:23:28 +0000

    Formatted the application to look nicer.
    There were a few indentation issues in the HTML portion of gen_slides, so I've fixed them up a bit.
    
    Signed-off-by: Victoria Lacroix <victoria.a.lacroix@gmail.com>

--- gen_slides.pl
+++ gen_slides.pl
@@ -14,71 +14,69 @@ my $resolution = '1280x720'; # as consumed by 'convert' command
 my @pics;
 
 my $header = <<HEAD;
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 
-        <!--
-                Supersized - Fullscreen Slideshow jQuery Plugin
-                Version : 3.2.6
-                Site    : www.buildinternet.com/project/supersized
+    <!--
+        Supersized - Fullscreen Slideshow jQuery Plugin
+        Version : 3.2.6
+        Site    : www.buildinternet.com/project/supersized
 
-                Author  : Sam Dunn
-                Company : One Mighty Roar (www.onemightyroar.com)
-                License : MIT License / GPL License
-        -->
+        Author  : Sam Dunn
+        Company : One Mighty Roar (www.onemightyroar.com)
+        License : MIT License / GPL License
+    -->
 
-        <head>
+    <head>
 
-                <title>J.N. Desmarais Library - Welcome</title>
-                <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+        <title>J.N. Desmarais Library - Welcome</title>
+        <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
 
-                <link rel="stylesheet" href="css/supersized.css" type="text/css" media="screen" />
-                <link rel="stylesheet" href="theme/supersized.shutter.css" type="text/css" media="screen" />
+        <link rel="stylesheet" href="css/supersized.css" type="text/css" media="screen" />
+        <link rel="stylesheet" href="theme/supersized.shutter.css" type="text/css" media="screen" />
 
-                <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js"></script>
-                <script type="text/javascript" src="js/jquery.easing.min.js"></script>
+        <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js"></script>
+        <script type="text/javascript" src="js/jquery.easing.min.js"></script>
 
-                <script type="text/javascript" src="js/supersized.3.2.6.min.js"></script>
-                <script type="text/javascript" src="theme/supersized.shutter.min.js"></script>
+        <script type="text/javascript" src="js/supersized.3.2.6.min.js"></script>
+        <script type="text/javascript" src="theme/supersized.shutter.min.js"></script>
 
-                <script type="text/javascript">
+        <script type="text/javascript">
 
-                        jQuery(function(\$){
+            jQuery(function(\$){
 
-                                \$.supersized({
+                \$.supersized({
 
-                                        // Functionality
-                                        fit_always              : 1, // scale down big images, scale up small images
-                                        slide_interval          :   $slide_interval,               // Length between transitions
-                                        transition              :   3,                  // 0-None, 1-Fade, 2-Slide Top, 3-Slide Right, 4-Slide Bottom, 5-Slide Left, 6-Carousel Right, 7-Carousel Left
-                                        transition_speed                :       700,            // Speed of transition
+                    // Functionality
+                    fit_always          : 1, // scale down big images, scale up small images
+                    slide_interval      : $slide_interval, // Length between transitions
+                    transition          : 3, // 0-None, 1-Fade, 2-Slide Top, 3-Slide Right, 4-Slide Bottom, 5-Slide Left, 6-Carousel Right, 7-Carousel Left
+                    transition_speed    : 700, // Speed of transition
 
-                                        // Components
-                                        slide_links                             :       "blank",        // Individual links for each slide (Options: false, "num", "name", "blank")
-                                        slides  : [
-';
+                    // Components
+                    slide_links         : "blank",  // Individual links for each slide (Options: false, "num", "name", "blank")
+                    slides  : [
 HEAD
 
 my $footer = <<FOOT;
-                          ]
 
-                                });
-                    });
+                    ]
+                });
+            });
+        </script>
+    </head>
 
-                </script>
+    <style type="text/css">
+        ul#demo-block{ margin:0 15px 15px 15px; }
+        ul#demo-block li{ margin:0 0 10px 0; padding:10px; display:inline; float:left; clear:both; color:#aaa; background:url("img/bg-black.png"); font:11px Helvetica, Arial, sans-serif; }
+        ul#demo-block li a{ color:#eee; font-weight:bold; }
+    </style>
 
-        </head>
+    <body>
 
-        <style type="text/css">
-                ul#demo-block{ margin:0 15px 15px 15px; }
-                        ul#demo-block li{ margin:0 0 10px 0; padding:10px; display:inline; float:left; clear:both; color:#aaa; background:url("img/bg-black.png"); font:11px Helvetica, Arial, sans-serif; }
-                        ul#demo-block li a{ color:#eee; font-weight:bold; }
-        </style>
-
-<body>
-
-</body>
+    </body>
 </html>
 FOOT
 
@@ -106,7 +104,7 @@ sub update_index {
     my $slides;
 
     foreach my $pic (@pics) {
-        $slides .= "\n{image : 'images/$pic'},";
+        $slides .= "{image : 'images/$pic'},\n";
     }
     chop($slides);
 
